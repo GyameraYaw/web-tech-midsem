7b76a192677c8da16f49f590a134b604918d9dda
Yaw Gyamera <yawgyamera@Yaws-MacBook-Air.local>
2024-11-05 12:51:19 +0000

    first commit

---
+++ JS/functionality.js
@@ -0,0 +1,11 @@
+let submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', function(){
+    alert('You have registered successfully!!')
+})
+
+
+function changeColor() {
+    let evenTitle = document.getElementById("eventTitle");
+    evenTitle.style.color = "green";
+}
